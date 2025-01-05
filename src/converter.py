6c98c4ca928085dabb9c72c3a80f19fb226af9f0
Sam Kyle <samjbkyle@gmail.com>
2025-01-05 20:36:56 +0000

    making json readable

--- src/converter.py
+++ src/converter.py
@@ -1,4 +1,6 @@
 import requests
+import json
+from pprint import pprint
 from .constants import POE_NINJA_URL, LEAGUE_NAME
 
 def get_current_rates():
@@ -9,8 +11,8 @@ def get_current_rates():
     
     response = requests.get(POE_NINJA_URL, params=params)
     
-    print(f"Status Code: {response.status_code}")
-    print(f"Response Content: {response.json()}")
+    pprint(f"Status Code: {response.status_code}")
+    pprint(f"Response Content: {response.json()}")
 
 if __name__ == "__main__":
     get_current_rates()
