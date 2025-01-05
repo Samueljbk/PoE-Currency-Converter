109640c2ffbe4b49a0386adbf3b01be78f9ae6f3
Sam Kyle <samjbkyle@gmail.com>
2025-01-05 14:55:35 +0000

    few todos

--- src/converter.py
+++ src/converter.py
@@ -2,4 +2,7 @@ import requests
 from .constants import POE_NINJA_URL, LEAGUE_NAME
 
 def get_current_rates():
+    # 1. Make API request to poe.ninja
+    # 2. Process the JSON response
+    # 3. Create and return dictionary of currency rates
     pass
